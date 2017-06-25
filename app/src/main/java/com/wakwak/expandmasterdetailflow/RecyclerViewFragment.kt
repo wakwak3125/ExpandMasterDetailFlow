0d3fa9970565a9e7cc2ed24718a9e9e06b10e2e8
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-06-25 14:25:13 +0900

    Create Fragment

---
+++ app/src/main/java/com/wakwak/expandmasterdetailflow/RecyclerViewFragment.kt
@@ -0,0 +1,13 @@
+package com.wakwak.expandmasterdetailflow
+
+import android.support.v4.app.Fragment
+
+/**
+ * Created by Ryo on 2017/06/25.
+ */
+class RecyclerViewFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RecyclerViewFragment()
+    }
+}
