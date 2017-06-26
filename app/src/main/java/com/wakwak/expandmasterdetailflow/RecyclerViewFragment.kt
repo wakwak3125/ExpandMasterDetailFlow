0deee81b1db61db82dec74b7a01c63abb9794776
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-06-26 17:43:59 +0900

    Set background image from center item

--- app/src/main/java/com/wakwak/expandmasterdetailflow/RecyclerViewFragment.kt
+++ app/src/main/java/com/wakwak/expandmasterdetailflow/RecyclerViewFragment.kt
@@ -5,9 +5,12 @@ import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.OrientationHelper
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.wakwak.expandmasterdetailflow.databinding.FragmentRecyclerViewBinding
 
 /**
@@ -17,6 +20,8 @@ class RecyclerViewFragment : Fragment() {
 
     private lateinit var binding: FragmentRecyclerViewBinding
     private val postRecyclerAdapter by lazy { PostRecyclerViewAdapter() }
+    private val layoutManager by lazy { LinearLayoutManager(context) }
+    private val verticalHelper by lazy { OrientationHelper.createVerticalHelper(layoutManager) }
 
     companion object {
         fun newInstance() = RecyclerViewFragment()
@@ -29,9 +34,11 @@ class RecyclerViewFragment : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = postRecyclerAdapter
         binding.recyclerView.addItemDecoration(divider())
+
+        binding.recyclerView.addOnScrollListener(scrollListener)
 
         updateAdapter()
     }
@@ -45,6 +52,51 @@ class RecyclerViewFragment : Fragment() {
     private fun divider(): DividerItemDecoration {
         return DividerItemDecoration(context, LinearLayoutManager.VERTICAL).apply {
             setDrawable(ContextCompat.getDrawable(context, R.drawable.rv_spacer))
+        }
+    }
+
+    private val scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val centerView: View? = findCenterView(layoutManager, verticalHelper)
+            centerView?.let { v ->
+                val bgImage = v.findViewById(R.id.background_image) as ImageView
+                binding.backgroundImage.setImageDrawable(bgImage.drawable)
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+        }
+
+        fun findCenterView(layoutManager: RecyclerView.LayoutManager,
+                           helper: OrientationHelper): View? {
+            val childCount = layoutManager.childCount
+            if (childCount == 0) {
+                return null
+            }
+
+            var closestChild: View? = null
+            val center: Int
+            if (layoutManager.clipToPadding) {
+                center = helper.startAfterPadding + helper.totalSpace / 2
+            } else {
+                center = helper.end / 2
+            }
+            var absClosest = Integer.MAX_VALUE
+
+            for (i in 0..childCount - 1) {
+                val child = layoutManager.getChildAt(i)
+                val childCenter = helper.getDecoratedStart(child) + helper.getDecoratedMeasurement(child) / 2
+                val absDistance = Math.abs(childCenter - center)
+
+                /** if child center is closer than previous closest, set it as closest   */
+                if (absDistance < absClosest) {
+                    absClosest = absDistance
+                    closestChild = child
+                }
+            }
+            return closestChild
         }
     }
 }
